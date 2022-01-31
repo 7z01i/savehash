720b95128e039ccae89b47d7cea8ed465119e181
tsoli <74723167+7z01i@users.noreply.github.com>
2022-01-31 19:01:11 +0100

    Add files via upload

--- savehash.sol
+++ savehash.sol
@@ -2,21 +2,21 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract SaveHash {
-    mapping(string => uint) public Hashes;
+    mapping(byte => uint) public Hashes;
 
-
+    private byte pin;
+    pin=0;
 
 //Function 1:
 //Save Hash with Transaction
 
-function set(string name, uint hash) private {
-        Hashes[name] = hash;
+function set(uint hash) private {
+        pin = pin +1;
+        Hashes[pin] = hash;
     }
 
 //Function 2: call Hash by Name
-function get(string name) public view returns (uint) {
-        // Mapping always returns a value.
-        // If the value was never set, it will return the default value.
+function get(string pin) public view returns (uint) {
         return Hashes[name];
     }
 
