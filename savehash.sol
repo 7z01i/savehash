2722622af3ae6f40c3a68128371147fd4d7a7231
moonfacebruh <39838632+moonfacebruh@users.noreply.github.com>
2022-02-04 19:33:46 +0100

    Update savehash.sol

--- savehash.sol
+++ savehash.sol
@@ -21,3 +21,80 @@ function get(string pin) public view returns (uint) {
     }
 
 }
+--------------- basic version: 
+WORKS
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract SaveHash {
+    
+    mapping( uint => bytes32) public Hashes; // mapping(keytype=> ValueType)
+
+    uint initialNumber=1000;
+
+    
+
+//Function 1:
+//Save Hash with Transaction +mapping+transmit pin to A
+
+    function saveHash( bytes32 Hash ) public returns (uint){
+        initialNumber=initialNumber+1;
+        Hashes[initialNumber] = Hash ;
+        return initialNumber;
+    }
+
+// Function last:
+// check the 2 hashes
+// need to add condition to be sure hashToVerify right format ?
+    function checkHash(uint initialNumber, bytes32 hashToVerify)public view returns (bool ) {
+	    if (Hashes[initialNumber]== hashToVerify){
+		    return true;
+        }
+       else{
+	       return false;
+       }
+   }
+
+}
+
+
+
+
+-------Version with mapping: key--> hash, Url: 
+NOT WORKING: HELP
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract SaveHash {
+    struct MyStruct {
+        bytes32 Hash;
+        string url;
+    }
+    mapping( uint => Mystruct) public Hashes; // mapping(keytype=> ValueType)
+    
+    uint initialNumber=1000;
+
+    
+
+//Function 1:
+//Save Hash with Transaction + transmit pin to A +mapping
+
+    function saveHash( bytes32 Hash, string memory url) public returns (uint){
+        initialNumber=initialNumber+1;
+        Hashes[initialNumber] = (Hash, url) ;
+        return initialNumber;
+    }
+
+// Function last:
+// check the 2 hashes
+// need to add condition to be sure hashToVerify right format ?
+    function checkHash(uint initialNumber, bytes32 hashToVerify)public view returns (bool ) {
+	    if (Hashes[initialNumber](0)]== hashToVerify){
+		    return true;
+        }
+       else{
+	       return false;
+       }
+   }
+
+}
